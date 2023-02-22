b32a21787f73fe518d8b4c43f9a2745b2264b27f
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-23 00:24:12 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+// console.log(galleryEl);
+
+const renderGallery = (pictures) => {
+    const result = pictures
+        .map((picture) => `<a class="gallery__item" href="${picture.original}">
+  <img class="gallery__image" src="${picture.preview}" alt="${picture.description}" />
+</a>`)
+        .join('');
+    return result;
+}
+// console.log(renderGallery(galleryItems));
+
+galleryEl.innerHTML = renderGallery(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionType: 'attr',
+    captionsData:  'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    scrollZoom: false,
+ });
