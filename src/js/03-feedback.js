cbac754248722378bd78ddd28975b4f5b7746f50
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-26 22:52:16 +0200

    finished task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,34 +3,42 @@ import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const refs = {
     form: document.querySelector(".feedback-form"),
-    email: document.querySelector(".email"),
+    email: document.querySelector(".feedback-form input"),
     textarea: document.querySelector(".feedback-form textarea"),
 };
 let formData = {};
 
+// Запускаємо слухачів подій
 refs.form.addEventListener("input", throttle(onFormInput, 500));
 refs.form.addEventListener("submit", onFormSubmit);
 
-populateTextarea();
+populateForm();
 
 function onFormInput(evt) {
     formData[evt.target.name] = evt.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
+
+// Команди при відправки форми Submit
+// Зупиняємо відправку форми за замовченням
+// Виводимо у консоль данні відправки
+// Очищюємо поля форми reset
+// Видаляємо дані зі сховища removeItem
 function onFormSubmit(evt) {
     evt.preventDefault();
-    console.log("Отправка формы, вывод объекта", formData);
+    console.log("Отправка формы, вывод объекта", JSON.parse(localStorage.getItem(STORAGE_KEY)));
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
-    formData = {}
 };
 
-function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+// Перевірка стану сховища.
+// Якщо  в сховищі є збережені дані - заповнити ними поля форми.
+function populateForm() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (savedMessage) {
-        const parsedMessage = JSON.parse(savedMessage);
-        refs.textarea.value = parsedMessage.message || '';
-        refs.email.value = parsedMessage.email || '';
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage.message || '';
+        refs.email.value = savedMessage.email || '';
     }
 }
 
